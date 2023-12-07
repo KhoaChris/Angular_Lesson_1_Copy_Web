e78b7f206178f30f77bbff25a3b1dc4e6e553763
Nguyen Tricsie <bddkhoa@gmail.com>
2023-12-07 15:27:49 +0700

    add component navbar

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavbarComponent { }
